a4f8a3c9ef89f1a6603a6a01f887d48a74ba2da3
Michael Chong <mikec0920@gmail.com>
2022-10-08 22:22:55 -0700

    Populate airports

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+airports = Airport.create([
+    { code: "ATL" }, { code: "DFW" }, { code: "DEN" }, { code: "ORD" }, { code: "LAX" },
+    { code: "CLT" }, { code: "MCO" }, { code: "LAS" }, { code: "PHX" }
+  ])
