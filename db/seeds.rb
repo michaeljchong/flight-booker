24b8b05d27122bb9631963b5ac83d0e64aa33829
Michael Chong <mikec0920@gmail.com>
2022-10-09 23:40:42 -0700

    Seed flight data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,7 @@ airports = Airport.create([
     { code: "ATL" }, { code: "DFW" }, { code: "DEN" }, { code: "ORD" }, { code: "LAX" },
     { code: "CLT" }, { code: "MCO" }, { code: "LAS" }, { code: "PHX" }
   ])
+
+flights = Flight.create([
+    { departure_airport_id: 2, arrival_airport_id: 1, departure_time: "Feb 22 2023 10:50", duration: "6:30" }
+  ])
