fa1a9a38fd2fd946197cad367662d6de193b798e
Michael Chong <mikec0920@gmail.com>
2022-10-10 22:42:15 -0700

    Update variable names

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,7 +2,7 @@ class FlightsController < ApplicationController
   def index
     @flights = Flight.all
     @airports = Airport.all.map{ |a| [a.code, a.id] }
-    @passengers = (1..4).to_a
-    @dates = @flights.map{ |f| [f.departure_time, f.id] }
+    @num_travelers = (1..4).to_a
+    @departure_times = @flights.map{ |f| [f.departure_time, f.id] }
   end
 end
