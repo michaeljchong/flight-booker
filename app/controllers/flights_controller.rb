38f5c09c0e5d8424065fee11406b0da6771e01d6
Michael Chong <mikec0920@gmail.com>
2022-10-10 22:35:11 -0700

    Create flight search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,8 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
+    @airports = Airport.all.map{ |a| [a.code, a.id] }
+    @passengers = (1..4).to_a
+    @dates = @flights.map{ |f| [f.departure_time, f.id] }
   end
 end
