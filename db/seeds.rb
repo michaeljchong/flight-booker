6686c6b4ec5a27f1d5a0b2cb4d5469c9a2655697
Michael Chong <mikec0920@gmail.com>
2022-10-10 22:34:33 -0700

    Update flight times

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,15 +17,15 @@ airports = Airport.create([
 # Seeded with fictional flights
 flights = Flight.create([
     { departure_airport_id: 2, arrival_airport_id: 1, departure_time: "Feb 22 2023 10:50", duration: "6:30" },
-    { departure_airport_id: 20, arrival_airport_id: 3, departure_time: "Feb 22 2023 10:50", duration: "6:30" },
-    { departure_airport_id: 15, arrival_airport_id: 16, departure_time: "Feb 22 2023 10:50", duration: "6:30" },
-    { departure_airport_id: 19, arrival_airport_id: 14, departure_time: "Feb 22 2023 10:50", duration: "6:30" },
-    { departure_airport_id: 3, arrival_airport_id: 5, departure_time: "Feb 22 2023 10:50", duration: "6:30" },
-    { departure_airport_id: 5, arrival_airport_id: 7, departure_time: "Feb 22 2023 10:50", duration: "6:30" },
-    { departure_airport_id: 7, arrival_airport_id: 3, departure_time: "Feb 22 2023 10:50", duration: "6:30" },
-    { departure_airport_id: 11, arrival_airport_id: 10, departure_time: "Feb 22 2023 10:50", duration: "6:30" },
-    { departure_airport_id: 13, arrival_airport_id: 18, departure_time: "Feb 22 2023 10:50", duration: "6:30" },
-    { departure_airport_id: 1, arrival_airport_id: 4, departure_time: "Feb 22 2023 10:50", duration: "6:30" },
-    { departure_airport_id: 16, arrival_airport_id: 9, departure_time: "Feb 22 2023 10:50", duration: "6:30" },
-    { departure_airport_id: 6, arrival_airport_id: 2, departure_time: "Feb 22 2023 10:50", duration: "6:30" }
+    { departure_airport_id: 20, arrival_airport_id: 3, departure_time: "Dec 3 2023 10:50", duration: "1:20" },
+    { departure_airport_id: 15, arrival_airport_id: 16, departure_time: "Jan 12 2023 10:50", duration: "8:39" },
+    { departure_airport_id: 19, arrival_airport_id: 14, departure_time: "Jan 19 2023 10:50", duration: "5:34" },
+    { departure_airport_id: 3, arrival_airport_id: 5, departure_time: "Mar 23 2023 10:50", duration: "7:52" },
+    { departure_airport_id: 5, arrival_airport_id: 7, departure_time: "Apr 10 2023 10:50", duration: "3:16" },
+    { departure_airport_id: 7, arrival_airport_id: 3, departure_time: "Apr 20 2023 10:50", duration: "0:59" },
+    { departure_airport_id: 11, arrival_airport_id: 10, departure_time: "Aug 2 2023 10:50", duration: "1:22" },
+    { departure_airport_id: 13, arrival_airport_id: 18, departure_time: "Oct 10 2023 10:50", duration: "7:18" },
+    { departure_airport_id: 1, arrival_airport_id: 4, departure_time: "Dec 6 2023 10:50", duration: "9:43" },
+    { departure_airport_id: 16, arrival_airport_id: 9, departure_time: "Dec 14 2023 10:50", duration: "17:06" },
+    { departure_airport_id: 6, arrival_airport_id: 2, departure_time: "Nov 8 2023 10:50", duration: "21:09" }
   ])
