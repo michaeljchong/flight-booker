fdb1b4ee9fed6373da7a3da35fce4dc245af4699
Michael Chong <mikec0920@gmail.com>
2022-10-11 18:06:35 -0700

    Sort airport codes

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,7 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
-    @airports = Airport.all.map{ |a| [a.code, a.id] }
+    @airports = Airport.all.map{ |a| [a.code, a.id] }.sort
     @num_travelers = (1..4).to_a
     @departure_times = @flights.map{ |f| [f.departure_time, f.id] }
   end
