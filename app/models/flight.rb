130cb37ccdb5f3de60d9edcf5ed6da70edbc8769
Michael Chong <mikec0920@gmail.com>
2022-10-11 18:00:27 -0700

    Format date selector options

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,12 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
+
+  def self.get_departure_times
+    departure_times = Flight.find_by_sql("select distinct departure_time from flights where departure_time is not null order by departure_time")
+  end 
+
+  def departure_date_formatted
+    departure_time.strftime("%m/%d/%Y")
+  end
 end
