76843e221f9a7f30f1a94e553aab6470f3304227
Michael Chong <mikec0920@gmail.com>
2022-10-12 22:48:54 -0700

    Refactor querying departure dates

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,7 +3,7 @@ class FlightsController < ApplicationController
     @flights = Flight.all
     @airports = Airport.all.map{ |a| [a.code, a.id] }.sort
     @num_travelers = (1..4).to_a
-    @departure_times = @flights.map{ |f| f.departure_time }
+    @departure_dates = @flights.map{ |f| f.departure_date_formatted }.sort
     @departure_airport = params[:departure_airport]
     @arrival_airport = params[:arrival_airport]
   end
