644f1cec31835e86c795d9619e1a3750140bbea6
Michael Chong <mikec0920@gmail.com>
2022-10-13 11:17:02 -0700

    Filter departure and arrival airport options

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,10 +1,13 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
-    @airports = Airport.all.map{ |a| [a.code, a.id] }.sort
-    @num_travelers = (1..4).to_a
+    @departure_airports = @flights.map{ |f| [f.departure_airport.code, f.departure_airport.id] }.sort
+    @arrival_airports = @flights.map{ |f| [f.arrival_airport.code, f.arrival_airport.id] }.sort
     @departure_dates = @flights.map{ |f| f.departure_date_formatted }.sort
-    @departure_airport = params[:departure_airport]
-    @arrival_airport = params[:arrival_airport]
+    @num_travelers = (1..4).to_a
+    
+    @chosen_departure_airport = params[:departure_airport]
+    @chosen_arrival_airport = params[:arrival_airport]
+    @chosen_departure_date = params[:departure_date] # search flights.departure_time for includes date
   end
 end
