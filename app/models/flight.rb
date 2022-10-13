a19c22058dd75b349170852f3eb9b1a601c692ba
Michael Chong <mikec0920@gmail.com>
2022-10-12 17:33:07 -0700

    Refactor method to scope

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,9 +2,7 @@ class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
 
-  def self.get_departure_times
-    departure_times = Flight.find_by_sql("select distinct departure_time from flights where departure_time is not null order by departure_time")
-  end 
+  scope :get_departure_times, -> { select(:departure_time).distinct.order(:departure_time) }
 
   def departure_date_formatted
     departure_time.strftime("%m/%d/%Y")
