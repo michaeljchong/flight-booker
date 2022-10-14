24c20536d0fd06bcfbdbfdee28bc19a5635e3a9c
Michael Chong <mikec0920@gmail.com>
2022-10-14 11:34:06 -0700

    Change fields to associative datatype

--- db/migrate/20221014042358_create_bookings.rb
+++ db/migrate/20221014042358_create_bookings.rb
@@ -1,8 +1,8 @@
 class CreateBookings < ActiveRecord::Migration[7.0]
   def change
     create_table :bookings do |t|
-      t.integer :flight_booking_id
-      t.integer :passenger_booking_id
+      t.belongs_to :booked_flight
+      t.belongs_to :booked_passenger
 
       t.timestamps
     end
