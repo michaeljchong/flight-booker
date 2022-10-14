21ccae56766de24de2b33ede6404bd077be03773
Michael Chong <mikec0920@gmail.com>
2022-10-13 22:09:54 -0700

    Edit field name

--- db/migrate/20221014042358_create_bookings.rb
+++ db/migrate/20221014042358_create_bookings.rb
@@ -1,8 +1,8 @@
 class CreateBookings < ActiveRecord::Migration[7.0]
   def change
     create_table :bookings do |t|
-      t.integer :flight_id
-      t.integer :passenger_id
+      t.integer :flight_booking_id
+      t.integer :passenger_booking_id
 
       t.timestamps
     end
