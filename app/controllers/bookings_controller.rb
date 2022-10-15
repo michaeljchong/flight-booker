17cf4c0d4c923b5d93d0674bf37e77426c0053c4
Michael Chong <mikec0920@gmail.com>
2022-10-14 18:24:55 -0700

    Create new action variables

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,8 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
+    @flight = Flight.find(params[:flight_id])
+    @num_travelers = params[:num_travelers].to_i
   end
 
   def create
