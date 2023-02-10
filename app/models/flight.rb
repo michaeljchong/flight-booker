48fe63f00193474b6c550199dde8b56141e8668a
Michael Chong <mikec0920@gmail.com>
2023-02-10 13:34:22 -0800

    Update bookings association to be deleted if flight is deleted

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,7 +1,7 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :passengers, through: :bookings
 
   scope :get_departure_times, -> { select(:departure_time).distinct.order(:departure_time) }
