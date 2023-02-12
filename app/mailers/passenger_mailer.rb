d61ee1d32f73d46fb53fad56384a5b096077c2f9
Michael Chong <mikec0920@gmail.com>
2023-02-11 23:16:12 -0800

    Generate passenger mailer

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,8 @@
 class PassengerMailer < ApplicationMailer
+  default from: "confirmation@flightbooker.com"
+
+  def confirmation_email
+    @passenger = params[:passenger]
+    mail(to: @passenger.email, subject: 'Booking Confirmed')
+  end
 end
